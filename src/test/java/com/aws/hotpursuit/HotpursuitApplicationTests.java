05a3993db58dfbbce63733f7e67a6bcd23b2567b
aron <aron@MacBook-Pro-de-Macbook.local>
2021-09-20 14:14:56 -0500

    test ci 07

--- src/test/java/com/aws/hotpursuit/HotpursuitApplicationTests.java
+++ src/test/java/com/aws/hotpursuit/HotpursuitApplicationTests.java
@@ -1,13 +1,33 @@
 package com.aws.hotpursuit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HotpursuitApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+
+    public void pingTest() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/ping")
+                .contentType(MediaType.ALL))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
